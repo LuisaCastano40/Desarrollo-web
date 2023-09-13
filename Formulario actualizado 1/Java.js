0db4bbaf297dfa10f60a6d09147746fb1471f20c
Luisa Castaño <luisa.castano40@eia.edu.co>
2023-09-13 13:54:41 -0500

    [UPDATE] deletion of several files

--- Formulario actualizado 1/Java.js
+++ Formulario actualizado 1/Java.js
@@ -1,7 +1,4 @@
 function darkMode(){
-    // let a = document.getElementsByTagName("body"); //selecciono al bodý porque me contiene todo
-    // a[0].style.backgroundColor = "black";
-    // a[0].style.color="white";
     const a= document.querySelector(".body-1").className="body-2";
     const b= document.querySelector(".contenedor-1").className="contenedor-1-2";
     const c= document.querySelector(".whatsapp").className="whatsapp-2";
@@ -14,3 +11,23 @@ function normalMode(){
     const c= document.querySelector(".whatsapp-2").className="whatsapp";
     console.log("normal Mode");
 }
+
+const button = document.querySelector(".btn");
+console.log(button);
+
+// button.addEventListener("click",function(evento) {
+//     evento.preventDefault();
+//     console.log("crear tareaaaaa");
+//     const input = document.querySelector("textarea");
+//     console.log(input.value);
+// })
+
+//Para código más limpio
+const createTask = (evento) => {
+    evento.preventDefault();
+    console.log("crear tareaaaaa");
+    const input = document.querySelector("textarea");
+    console.log(input.value);
+}
+button.addEventListener("click", createTask);
+window.alert("Hola mundo");
